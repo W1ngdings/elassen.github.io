12a946be03caf4f0cf76f79f8a6a41e7f024a39a
W1ngdings <emil35126@hotmail.com>
2025-09-13 22:15:51 +0200

    Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
+import styles from '../components/Home.module.css';
+
+
 
 export default function Home() {
   return (
-    <div className="about">
-      <h1>About Me</h1>
+    <div className={styles.home}>
+      <h1>Homepage</h1>
     </div>
   );
+}
+
+function HomeStyle() {
+  return <home className={styles.home}>This is the home page</home>;
 }
