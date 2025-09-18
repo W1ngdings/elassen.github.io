e7460e0ed2d6c75b342ca80e52ed8ab585fddbc5
W1ngdings <emil35126@hotmail.com>
2025-09-18 02:15:58 +0200

    commit

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,27 +7,27 @@ export default function Projects() {
       <h1 className={styles.introtext}>My previous projects</h1>
 
       <div className={styles.row}>
-        <img className={styles.image} src="src/assets/gamedify-img.webp" alt="" />
+        <img className={styles.image} src="public/images/gamedify-img.webp" alt="" />
         <p></p>
       </div>
 
       <div className={styles.row}>
-        <img className={styles.image} src="src/assets/dengulecafe-img.webp" alt="" />
+        <img className={styles.image} src="public/images/dengulecafe-img.webp" alt="" />
         <p></p>
       </div>
 
       <div className={styles.row}>
-        <img className={styles.image} src="src/assets/radar-img.webp" alt="" />
+        <img className={styles.image} src="public/images/radar-img.webp" alt="" />
         <p></p>
       </div>
 
       <div className={styles.row}>
-        <img className={styles.image} src="src/assets/zerobuzz-img.webp" alt="" />
+        <img className={styles.image} src="public/images/zerobuzz-img.webp" alt="" />
         <p></p>
       </div>
 
       <div className={styles.row}>
-        <img className={styles.image} src="src/assets/kenderdubrysterne-img.webp" alt="" />
+        <img className={styles.image} src="public/images/kenderdubrysterne-img.webp" alt="" />
         <p></p>
       </div>
     </div>
